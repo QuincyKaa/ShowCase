8c10697a77c59764ca205e0fc33efe9a2eda841f
Quincy van de Kaa <s1184597@student.windesheim.nl>
2024-02-26 10:41:26 +0100

    eslint installed

--- js/app.js
+++ js/app.js
@@ -1,119 +1,111 @@
 class GDPR {
+  constructor () {
+    this.bindEvents()
+    if (this.cookieStatus() !== 'accept') this.showGDPR()
+  }
 
-    constructor() {
-        this.bindEvents();
-        if(this.cookieStatus() !== 'accept') this.showGDPR();
+  bindEvents () {
+    const buttonAccept = document.querySelector('.gdpr-consent__button--accept')
+    buttonAccept.addEventListener('click', () => {
+      this.cookieStatus('accept')
+      this.hideGDPR()
+    })
+
+    const buttonReject = document.querySelector('.gdpr-consent__button--reject') // student uitwerking
+    buttonReject.addEventListener('click', () => {
+      this.cookieStatus('reject')
+      this.hideGDPR()
+    })
+  }
+
+  cookieStatus (status) {
+    if (status) {
+      localStorage.setItem('gdpr-consent-choice', status)
+      localStorage.setItem('gdpr-consent-metadata', this.getMetadataJSON()) // student uitwerking methode aanroepen
     }
 
-    bindEvents() {
-        let buttonAccept = document.querySelector('.gdpr-consent__button--accept');
-        buttonAccept.addEventListener('click', () => {
-            this.cookieStatus('accept');
-            this.hideGDPR();
-        });
+    return localStorage.getItem('gdpr-consent-choice')
+  }
 
-        let buttonReject = document.querySelector('.gdpr-consent__button--reject');  //student uitwerking
-        buttonReject.addEventListener('click',()=>{
-            this.cookieStatus('reject');
-            this.hideGDPR();
-        });
+  getMetadataJSON () { // student uitwerking methode met datum en tijd -     //'Date':'dd-mm-jjjj','Time:'hh:mm'
+    const date = new Date()
+    return JSON.stringify({ datum: `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`, tijd: `${date.getHours()}:${date.getMinutes()}` })
+  }
 
-    }
+  hideGDPR () {
+    document.querySelector('.gdpr-consent').classList.add('hide')
+    document.querySelector('.gdpr-consent').classList.remove('show')
+  }
 
-    cookieStatus(status) {
-
-        if (status) 
-        {
-            localStorage.setItem('gdpr-consent-choice', status);
-            localStorage.setItem('gdpr-consent-metadata',this.getMetadataJSON());    //student uitwerking methode aanroepen
-        }
-
-        return localStorage.getItem('gdpr-consent-choice');
-    }
-
-    
-    getMetadataJSON(){    //student uitwerking methode met datum en tijd -     //'Date':'dd-mm-jjjj','Time:'hh:mm'
-        let date = new Date();
-        return JSON.stringify({ datum: `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`, tijd: `${date.getHours()}:${date.getMinutes()}` })
-    }
-
-    
-    hideGDPR(){
-        document.querySelector(`.gdpr-consent`).classList.add('hide');
-        document.querySelector(`.gdpr-consent`).classList.remove('show');
-    }
-
-    showGDPR(){
-        document.querySelector(`.gdpr-consent`).classList.add('show');
-    }
-
+  showGDPR () {
+    document.querySelector('.gdpr-consent').classList.add('show')
+  }
 }
 
-const gdpr = new GDPR();
+const gdpr = new GDPR()
 
 class Validation {
-    constructor() {
-        this.validationResults = {
-            firstname: false,
-            lastname: false,
-            phone_number: false,
-            email: false,
-            subject: false,
-            message: false,
-        };
-
-        this.setupEventListeners();
+  constructor () {
+    this.validationResults = {
+      firstname: false,
+      lastname: false,
+      phone_number: false,
+      email: false,
+      subject: false,
+      message: false
     }
 
-    setupEventListeners() {
-        const fields = ["firstname", "lastname", "phone_number", "subject", "message"];
+    this.setupEventListeners()
+  }
 
-        fields.forEach(field => {
-            const item = document.getElementById(field);
-            item.addEventListener("input", () => {
-                if (!item.checkValidity()) {
-                    item.reportValidity();
-                    this.validationResults[field] = false;
-                } else {
-                    item.setCustomValidity("");
-                    this.validationResults[field] = true;
-                }
-                this.updateSendButton();
-            });
-        });
+  setupEventListeners () {
+    const fields = ['firstname', 'lastname', 'phone_number', 'subject', 'message']
 
-        // Email validation logic using browser's built-in validation
-        const email = document.getElementById("email");
-        email.addEventListener("input", () => {
-            if (!email.checkValidity()) {
-                if (email.value.indexOf('@') === -1) {
-                    email.setCustomValidity(`Vul een geldig e-mail adres in van ${email.minLength} tot en met ${email.maxLength} karakters`);
-                } else {
-                    email.setCustomValidity("");
-                }
-                email.reportValidity();  // Explicitly call reportValidity to show the custom validity message
-                this.validationResults.email = false;
-            } else {
-                email.setCustomValidity("");
-                this.validationResults.email = true;
-            }
-            this.updateSendButton();
-        });
-    }
-
-    /* checks if the button can be enabled */
-    updateSendButton() {
-        let sendButton = document.getElementById("sendButton");
-
-        // Enable the button if all conditions are true, otherwise disable it
-        const isValid = Object.values(this.validationResults).every(result => result === true);
-        if (isValid) {
-            sendButton.removeAttribute("disabled");
+    fields.forEach(field => {
+      const item = document.getElementById(field)
+      item.addEventListener('input', () => {
+        if (!item.checkValidity()) {
+          item.reportValidity()
+          this.validationResults[field] = false
         } else {
-            sendButton.setAttribute("disabled", "disabled");
+          item.setCustomValidity('')
+          this.validationResults[field] = true
         }
+        this.updateSendButton()
+      })
+    })
+
+    // Email validation logic using browser's built-in validation
+    const email = document.getElementById('email')
+    email.addEventListener('input', () => {
+      if (!email.checkValidity()) {
+        if (email.value.indexOf('@') === -1) {
+          email.setCustomValidity(`Vul een geldig e-mail adres in van ${email.minLength} tot en met ${email.maxLength} karakters`)
+        } else {
+          email.setCustomValidity('')
+        }
+        email.reportValidity() // Explicitly call reportValidity to show the custom validity message
+        this.validationResults.email = false
+      } else {
+        email.setCustomValidity('')
+        this.validationResults.email = true
+      }
+      this.updateSendButton()
+    })
+  }
+
+  /* checks if the button can be enabled */
+  updateSendButton () {
+    const sendButton = document.getElementById('sendButton')
+
+    // Enable the button if all conditions are true, otherwise disable it
+    const isValid = Object.values(this.validationResults).every(result => result === true)
+    if (isValid) {
+      sendButton.removeAttribute('disabled')
+    } else {
+      sendButton.setAttribute('disabled', 'disabled')
     }
+  }
 }
 
-const validation = new Validation();
-
+const validation = new Validation()
